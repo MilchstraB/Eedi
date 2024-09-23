c534f3002968ccf0a130fa8791a762f8ee7a465f
MilchstraB <523169432@qq.com>
2024-09-24 00:54:01 +0800

    update _save()

--- retrieval/Embedding_trainer.py
+++ retrieval/Embedding_trainer.py
@@ -1,3 +1,5 @@
+import os
+import logging
 from tqdm import tqdm
 from typing import Optional, List, Dict
 
@@ -6,6 +8,8 @@ import torch.distributed as dist
 from torch.utils.data import DataLoader, Dataset
 from transformers.trainer import Trainer
 from transformers.trainer_utils import EvalLoopOutput
+
+logger = logging.getLogger(__name__)
 
 
 class RetrievalTrainer(Trainer):
@@ -61,3 +65,16 @@ class RetrievalTrainer(Trainer):
         self._memory_tracker.stop_and_update_metrics(output.metrics)
 
         return output.metrics
+
+    def _save(self, output_dir: Optional[str] = None, state_dict=None):
+        # If we are executing this function, we are the process zero, so we don't check for that.
+        output_dir = output_dir if output_dir is not None else self.args.output_dir
+        os.makedirs(output_dir, exist_ok=True)
+        logger.info(f"Saving model checkpoint to {output_dir}")
+
+        self.model.model.save_pretrained(
+            output_dir, state_dict=state_dict, safe_serialization=self.args.save_safetensors
+        )
+
+        if self.tokenizer is not None:
+            self.tokenizer.save_pretrained(output_dir)
