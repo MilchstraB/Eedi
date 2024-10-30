d414eab8f5e2eccd68b642aa334c4d766a8e9c46
MilchstraB <523169432@qq.com>
2024-10-30 20:43:24 +0800

    parse_data

---
+++ text_processor/parse_raw_data.py
@@ -0,0 +1,56 @@
+import re
+import json
+import pandas as pd
+
+
+def parse_data(raw_data, mode="llama"):
+    assert mode in ["llama", "mistral"], "Invalid mode. Choose between 'llama' and 'mistral'."
+
+    brief_description_pattern = re.compile(r"Brief description of the concept or skill being tested: (.*?)Question:", re.DOTALL)
+    question_pattern = re.compile(r"Question: (.*?)Correct answer:", re.DOTALL)
+    correct_answer_pattern = re.compile(r"Correct answer: (.*?)Incorrect answer:", re.DOTALL)
+
+    if mode == "llama":
+        incorrect_answer_pattern = re.compile(r"Incorrect answer: (.*?)What is the misconception", re.DOTALL)
+        response_a_pattern = re.compile(r"\[RESPONSE A\] (.*?)\[RESPONSE B\]", re.DOTALL)
+        response_b_pattern = re.compile(r"\[RESPONSE B\] (.*)", re.DOTALL)
+    elif mode == "mistral":
+        incorrect_answer_pattern = re.compile(r"Incorrect answer: (.*?)\[MISCONCEPTION A\]", re.DOTALL)
+        response_a_pattern = re.compile(r"\[MISCONCEPTION A\] (.*?)\[MISCONCEPTION B\]", re.DOTALL)
+        response_b_pattern = re.compile(r"\[MISCONCEPTION B\] (.*)", re.DOTALL)       
+    
+    parse_data = []
+
+    for item in raw_data:
+        text = item[0]["content"]
+        preferences = item[1]["content"]
+
+        brief_description = brief_description_pattern.search(text).group(1).strip()
+        question = question_pattern.search(text).group(1).strip()
+        correct_answer = correct_answer_pattern.search(text).group(1).strip()
+        incorrect_answer = incorrect_answer_pattern.search(text).group(1).strip()
+        winner = response_a_pattern.search(text).group(1).strip()
+        loser = response_b_pattern.search(text).group(1).strip()
+
+        if preferences == "B": winner, loser = loser, winner
+        parse_data.append({
+            "brief_description": brief_description,
+            "question": question,
+            "correct_answer": correct_answer,
+            "incorrect_answer": incorrect_answer,
+            "positive": winner,
+            "negative": loser,
+        })
+
+    return parse_data
+
+
+if __name__ == "__main__":
+    dest_path = "data/pair_preference_eedi_mistral.json"
+    raw_data = pd.read_parquet("data/pair_preference_eedi_mistral.parquet")
+    mode = "mistral"
+
+    raw_data = raw_data["messages"].to_list()
+    parse_data = parse_data(raw_data, mode)
+    with open(dest_path, "w") as file:
+        json.dump(parse_data, file, indent=4)
