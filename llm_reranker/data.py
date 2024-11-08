b784948de360ffb49c14737018fc75b242a9e1e2
MilchstraB <523169432@qq.com>
2024-11-08 16:23:30 +0800

    val for llm reranker

--- llm_reranker/data.py
+++ llm_reranker/data.py
@@ -1,5 +1,6 @@
 import os
 import random
+import pandas as pd
 from typing import List, Tuple, Dict
 from dataclasses import dataclass
 
@@ -68,3 +69,70 @@ class GroupCollator(DataCollatorWithPadding):
         if isinstance(features[0], list):
             features = sum(features, [])
         return super().__call__(features)
+    
+
+class ValDatasetForRerank(Dataset):
+    def __init__(self, args, tokenizer: AutoTokenizer):
+        self.dataset = datasets.load_dataset('json', data_files=args.val_data_path, split='train')
+
+        misconceptions_map = pd.read_csv(args.misconception_mapping)
+        self.misconception_map = {
+            row["MisconceptionName"]: row["MisconceptionId"]
+            for _, row in misconceptions_map.iterrows()
+        } 
+
+        self.tokenizer = tokenizer
+        self.args = args
+        self.total_len = len(self.dataset)
+        self.template = args.template
+        self.prompt = args.prompt
+        self.max_length = args.max_length - 1 if self.tokenizer.bos_token_id else args.max_length
+
+    def __len__(self):
+        return self.total_len
+
+    def __getitem__(self, item) -> List[BatchEncoding]:
+        query = self.dataset[item]['query']
+        candidates = self.dataset[item]['candidates']
+
+        # Encoding
+        inputs, mis_ids = [], []
+        prompt = self.tokenizer(
+            self.prompt + "Yes",
+            return_tensors=None,
+            add_special_tokens=False,
+        )["input_ids"]
+
+        for passage in candidates:
+            mis_ids.append(self.misconception_map[passage])
+            text = self.template.format_map({"query": query, "passage": passage})
+            encoding = self.tokenizer(
+                text,
+                max_length=self.max_length - len(prompt),
+                padding=False,
+                truncation=True,
+            )["input_ids"]
+            encoding = [self.tokenizer.bos_token_id] + encoding + prompt \
+                if self.tokenizer.bos_token_id else encoding + prompt
+            attention_mask = [1] * len(encoding)
+            label = [-100] * (len(encoding) - 1) + [encoding[-1]]
+            inputs.append({"input_ids": encoding, "attention_mask": attention_mask, "labels": label})
+
+        return inputs, mis_ids
+
+
+@dataclass
+class ValCollator(DataCollatorWithPadding):
+    def __call__(
+        self, features
+    ) -> Tuple[Dict[str, torch.Tensor], Dict[str, torch.Tensor]]:
+        batch_data = [f[0] for f in features]
+        mis_ids = [f[1] for f in features]
+
+        if isinstance(batch_data[0], list):
+            batch_data = sum(batch_data, [])
+        if isinstance(mis_ids[0], list):
+            mis_ids = sum(mis_ids, [])
+
+        batch_data = self.tokenizer.pad(batch_data, padding="longest", return_tensors="pt")
+        return {"inputs": batch_data, "mis_ids": mis_ids}
